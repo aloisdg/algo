fb9b4c47cce17de031eea4f9dec8a6c85366eade
aloisdg <aloisdegouvello@live.fr>
2014-03-21 03:16:20 +0100

    init Levenshtein

---
+++ Algo/Levenshtein/Levenshtein.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Levenshtein
+{
+    public static class Levenshtein
+    {
+        public static int FindDistance(string s, string t)
+        {
+            return 1;
+        }
+    }
+}

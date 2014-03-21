d68de48007bbee45df108b4c74a16da9fd4c0848
aloisdg <aloisdegouvello@live.fr>
2014-03-21 04:18:30 +0100

    printed some info

--- Algo/Algo/Program.cs
+++ Algo/Algo/Program.cs
@@ -10,6 +10,9 @@ namespace Algo
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("This solution is a kind of exercice.");
+            Console.WriteLine("I am going to reimplement some classics algorithms.");
+            Console.WriteLine("It may not the fastest, cleanest, or prettiest algo of their kind.");
         }
     }
 }

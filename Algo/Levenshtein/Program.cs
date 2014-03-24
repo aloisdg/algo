c19f10b5e0cdea487bc2fe49f2d65e721db32883
aloisdg <aloisdegouvello@live.fr>
2014-03-25 00:51:43 +0100

    edited example

--- Algo/Levenshtein/Program.cs
+++ Algo/Levenshtein/Program.cs
@@ -11,7 +11,7 @@ namespace Levenshtein
         static void Main(string[] args)
         {
             Console.WriteLine(Levenshtein.FindDistance("Alois", "aloisdg"));
-            Console.WriteLine(Levenshtein.FindDistance("Alois", "aloisdg"));
+            Console.WriteLine(Levenshtein.FindDistance("Alois", "aloisdg", true));
             Console.ReadLine();
         }
     }

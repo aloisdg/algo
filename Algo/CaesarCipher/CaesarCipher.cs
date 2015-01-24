9a2e22079074562e910d9d0224773e402ef97d27
aloisdg <aloisdegouvello@live.fr>
2015-01-24 01:52:58 +0100

    Add cipher

---
+++ Algo/CaesarCipher/CaesarCipher.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaesarCipher
+{
+    public class CaesarCipher
+    {
+        /*
+         * http://en.wikipedia.org/wiki/Caesar_cipher
+         */
+
+        public static string Encrypt(string input, int code)
+        {
+            return RunCipher(input, code);
+        }
+
+        public static string Decrypt(string input, int code)
+        {
+            return RunCipher(input, -code);
+        }
+
+        private static string RunCipher(string letters, int shift)
+        {
+            return new String(MoveLetters(letters, shift).ToArray());
+        }
+
+        public static IEnumerable<char> MoveLetters(string letters, int shift)
+        {
+            return from letter in letters
+                   let offset = Char.IsUpper(letter) ? 'A' : 'a'
+                   select Char.IsLetter(letter) ? (char)(((letter + shift - offset) % 26) + offset) : letter;
+        }
+    }
+}
---
+++ Algo/CaesarCipher/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaesarCipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "je t'aime";
+            str = CaesarCipher.Encrypt(str, 6);
+            Console.WriteLine("Encrypted: {0}", str);
+            str = CaesarCipher.Decrypt(str, 6);
+            Console.WriteLine("Decrypted: {0}", str);
+
+            str = "je t'aime";
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine(str == CaesarCipher.Decrypt(CaesarCipher.Encrypt(str, i), i)
+                    ? "{0} ok" : "{0} ko", i);
+            }
+            Console.ReadLine();
+        }
+    }
+}

2e89ecc869394a7e8297222c69f3a0398a46f383
aloisdg <aloisdegouvello@live.fr>
2015-05-27 00:09:54 +0200

    Add happy number

---
+++ Algo/HappyNumber/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HappyNumber
+{
+	class Program
+	{
+		static void Main()
+		{
+			for (var i = 0; i < 100; i++)
+				if (i.IsHappy())
+					Console.Write(i + " ");
+			Console.ReadLine();
+		}
+	}
+}
